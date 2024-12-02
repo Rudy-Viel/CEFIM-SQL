16fd21c67185f7f2208ee15f2e66be8245bc4a88
VIEL RUDY <rudy06102001@gmail.com>
2024-12-02 10:10:54 +0100

    Remove insert from tp 01

--- TP-01.sql
+++ TP-01.sql
@@ -12,9 +12,3 @@ CREATE TABLE IF NOT EXISTS chat (
     yeux VARCHAR(50) NOT NULL,
     age INT NOT NULL
 );
-
-INSERT INTO chat (id, nom, yeux, age) VALUES 
-(1, 'Maine coon', 'marron', 20),
-(2, 'Siamois', 'bleu', 15),
-(3, 'Bengal', 'marron', 18),
-(4, 'Scottish Fold', 'marron', 10);
